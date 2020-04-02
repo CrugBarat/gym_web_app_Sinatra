4b99b201382a1628a991413862dd34aa9524f68f
Craig Barratt <craigtbarratt@gmail.com>
2020-04-03 00:36:01 +0100

    adds contructor to the ClassDateTime class

---
+++ models/class_date_time.rb
@@ -0,0 +1,16 @@
+require_relative('../db/sql_runner.rb')
+
+class ClassDateTime
+
+  attr_reader :id
+  attr_accessor :start_time, :end_time, :room_id, :class_date, :class_date
+
+  def initialize(options)
+    @id = options['id'].to_i if options['id']
+    @start_time = options['start_time']
+    @end_time = options['end_time']
+    @class_date = options['class_date']
+    @class_id = options['class_id'].to_i
+  end
+
+end
