aa6c1472f0f3c6971f08f0ca9415c254b902fc50
Craig Barratt <craigtbarratt@gmail.com>
2020-04-03 01:53:13 +0100

    adds CRUD to Member model

--- models/member.rb
+++ models/member.rb
@@ -13,4 +13,49 @@ class Member
     @active = options['active']
   end
 
+  def save()
+    sql = "INSERT INTO members
+    (first_name, last_name, date_of_birth, active)
+    VALUES ($1, $2, $3, $4)
+    RETURNING *"
+    values = [@first_name, @last_name, @date_of_birth, @active]
+    @id = SqlRunner.run(sql, values)[0]['id'].to_i
+  end
+
+  def self.all()
+    sql = "SELECT * FROM members"
+    result = SqlRunner.run(sql)
+    self.map_items(result)
+  end
+
+  def update()
+    sql = "UPDATE members
+    SET (first_name, last_name, date_of_birth, active)
+    = ($1, $2, £3, $4)
+    WHERE id = $5"
+    values = [@first_name, @last_name, @date_of_birth, @active, @id]
+    SqlRunner.run(sql, values)
+  end
+
+  def delete()
+    sql = "DELETE FROM members
+    WHERE id = $1"
+    values = [@id]
+    SqlRunner.run(sql, values)
+  end
+
+  def self.delete_all()
+    sql = "DELETE FROM members"
+    SqlRunner.run(sql)
+  end
+
+  def self.map_items(result)
+    result.map{|member| Member.new(member)}
+  end
+
+  def self.returns_single_member(results)
+    return nil if results.first() == nil
+    Member.new(results.first())
+  end
+
 end
