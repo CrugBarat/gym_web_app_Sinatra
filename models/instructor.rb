70b50543837b7551ec11ed4b15266d87c9c3fa22
Craig Barratt <craigtbarratt@gmail.com>
2020-04-03 02:38:50 +0100

    adds find_by_id to Instructor model

--- models/instructor.rb
+++ models/instructor.rb
@@ -49,6 +49,14 @@ class Instructor
     SqlRunner.run(sql)
   end
 
+  def self.find_by_id(id)
+    sql = "SELECT * FROM instructors
+           WHERE id = $1"
+    values = [id]
+    results = SqlRunner.run(sql, values)
+    self.returns_single_instructor(results)
+  end
+
   def self.map_items(result)
     result.map{|instructor| Instructor.new(instructor)}
   end
