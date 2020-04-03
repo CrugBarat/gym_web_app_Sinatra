0ef52b5e19c2d530ebe597478c63c876faba745b
Craig Barratt <craigtbarratt@gmail.com>
2020-04-03 11:46:13 +0100

    adds show all classes to instructor_controller

--- controllers/instructors_controller.rb
+++ controllers/instructors_controller.rb
@@ -10,3 +10,12 @@ get '/instructors/members' do
   @members = Member.all()
   erb(:"instructors/members")
 end
+
+get '/instructors/classes' do
+  @classes = Classes.all()
+  erb(:"instructors/classes")
+end
+
+get '/instructors/members/create' do
+  erb(:"instructors/members_create")
+end
