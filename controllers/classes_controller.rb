00896fa063d1d57ba9c050a803cd3a8ff180145d
Craig Barratt <craigtbarratt@gmail.com>
2020-04-03 03:37:48 +0100

    adds members/classes display on respective app pages

--- controllers/classes_controller.rb
+++ controllers/classes_controller.rb
@@ -1,6 +1,8 @@
 require('sinatra')
 require('sinatra/contrib/all') if development?
+require_relative('../models/classes.rb')
 
 get '/classes' do
+  @classes = Classes.all()
   erb(:"classes/home")
 end
--- controllers/instructors_controller.rb
+++ controllers/instructors_controller.rb
@@ -1,6 +1,12 @@
 require('sinatra')
 require('sinatra/contrib/all') if development?
+require_relative('../models/member.rb')
 
 get '/instructors' do
   erb(:"instructors/home")
+end
+
+get '/instructors/members' do
+  @members = Member.all()
+  erb(:"instructors/members")
 end
