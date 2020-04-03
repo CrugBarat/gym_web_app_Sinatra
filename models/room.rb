b946e86416788c9c65b5a38a97a9a60e1260aa4c
Craig Barratt <craigtbarratt@gmail.com>
2020-04-03 01:55:55 +0100

    adds CRUD to Room model

--- models/room.rb
+++ models/room.rb
@@ -10,4 +10,49 @@ class Room
     @name = options['name']
   end
 
+  def save()
+    sql = "INSERT INTO rooms
+    (first_name)
+    VALUES ($1)
+    RETURNING *"
+    values = [@first_name]
+    @id = SqlRunner.run(sql, values)[0]['id'].to_i
+  end
+
+  def self.all()
+    sql = "SELECT * FROM rooms"
+    result = SqlRunner.run(sql)
+    self.map_items(result)
+  end
+
+  def update()
+    sql = "UPDATE rooms
+    SET (first_name)
+    = ($1)
+    WHERE id = $2"
+    values = [@first_name, @id]
+    SqlRunner.run(sql, values)
+  end
+
+  def delete()
+    sql = "DELETE FROM rooms
+    WHERE id = $1"
+    values = [@id]
+    SqlRunner.run(sql, values)
+  end
+
+  def self.delete_all()
+    sql = "DELETE FROM rooms"
+    SqlRunner.run(sql)
+  end
+
+  def self.map_items(result)
+    result.map{|room| Room.new(room)}
+  end
+
+  def self.returns_single_room(results)
+    return nil if results.first() == nil
+    Room.new(results.first())
+  end
+
 end
