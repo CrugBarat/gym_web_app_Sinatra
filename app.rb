b2a0d7fcc62ed51a5a115997e4e921d8af6c471f
CrugBarat <craigtbarratt@gmail.com>
2020-05-21 15:39:54 +0100

    setting up heroku
    Signed-off-by: CrugBarat <craigtbarratt@gmail.com>

--- app.rb
+++ app.rb
@@ -2,6 +2,21 @@ require( 'sinatra' )
 require( 'sinatra/contrib/all' ) if development?
 require_relative('controllers/admin/admin_controller.rb')
 require_relative('controllers/users/users_controller.rb')
+require_relative('./members_controller.rb')
+require_relative('./members_details_controller.rb')
+require_relative('./sessions_controller.rb')
+require_relative('./bookings_controller.rb')
+require_relative('./session_dates_times_controller.rb')
+require_relative('./instructors_controller.rb')
+require_relative('./rooms_controller.rb')
+require_relative('./equipment_controller.rb')
+require_relative('./members_controller.rb')
+require_relative('./members_details_controller.rb')
+require_relative('./sessions_controller.rb')
+require_relative('./bookings_controller.rb')
+require_relative('./instructors_controller.rb')
+require_relative('./rooms_controller.rb')
+require_relative('./calendar_controller.rb')
 
 set :raise_errors, false
 set :show_exceptions, false
