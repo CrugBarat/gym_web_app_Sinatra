39e69572f572f9be95b4cc4bf452f8276e86818c
CrugBarat <craigtbarratt@gmail.com>
2020-05-21 15:52:49 +0100

    tests heroku multi controller bug fix
    Signed-off-by: CrugBarat <craigtbarratt@gmail.com>

--- app.rb
+++ app.rb
@@ -1,7 +1,22 @@
 require( 'sinatra' )
 require( 'sinatra/contrib/all' ) if development?
 require_relative('controllers/admin/admin_controller.rb')
+require_relative('controllers/admin/members_controller.rb')
+require_relative('controllers/admin/members_details_controller.rb')
+require_relative('controllers/admin/sessions_controller.rb')
+require_relative('controllers/admin/bookings_controller.rb')
+require_relative('controllers/admin/session_dates_times_controller.rb')
+require_relative('controllers/admin/instructors_controller.rb')
+require_relative('controllers/admin/rooms_controller.rb')
+require_relative('controllers/admin/equipment_controller.rb')
 require_relative('controllers/users/users_controller.rb')
+require_relative('controllers/users/members_controller.rb')
+require_relative('controllers/users/members_details_controller.rb')
+require_relative('controllers/users/sessions_controller.rb')
+require_relative('controllers/users/bookings_controller.rb')
+require_relative('controllers/users/instructors_controller.rb')
+require_relative('controllers/users/rooms_controller.rb')
+require_relative('controllers/users/calendar_controller.rb')
 
 
 set :raise_errors, false
